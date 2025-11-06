3ccb1cbc9eba4dd41692691f0ef624cf3c35ff4e
yunjin25-hue <yunjin25@sookmyung.ac.kr>
2025-11-06 15:18:25 +0900

    1

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+ 
+  char code1='A';
+  int code2=65;
+  
+  printf("code1=%c,(%d)\n",code1,code1);
+  printf("code2=%c,(%d)\n",code2,code2);
+   
+  system("PAUSE");	
+  return 0;
+}
